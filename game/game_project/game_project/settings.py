438963667c1af6d74be0f1de895677bb87247f3b
sepi_En <sepehrbaghri1385.1386@Email.com>
2026-07-20 08:58:24 -0700

    Add media settings

--- game/game_project/game_project/settings.py
+++ game/game_project/game_project/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'game / game_project' / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # -----------------------------

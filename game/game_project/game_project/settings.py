6d570edca8c448466b423d232fc27863c2fc5495
sepi_En <sepehrbaghri1385.1386@Email.com>
2026-07-21 09:26:52 -0700

    Fix static files

--- game/game_project/game_project/settings.py
+++ game/game_project/game_project/settings.py
@@ -58,12 +58,11 @@ INSTALLED_APPS = [
     'login_app',
     'new_game_app',
     'coment_app',
-    'shop_app'
+    'shop_app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -139,9 +138,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'Game_project' / 'static',
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # -----------------------------
